079883dc4c1d525a065c857b6cfe5c6ad57138f2
tienpm <tienpm@bkhightech.vn>
2024-04-22 14:41:23 +0700

    save

--- ppocr/data/simple_dataset.py
+++ ppocr/data/simple_dataset.py
@@ -148,6 +148,7 @@ class SimpleDataSet(Dataset):
             file_name = substr[0]
             file_name = self._try_parse_filename_list(file_name)
             label = substr[1]
+            print('self.data_dir', self.data_dir)
             img_path = os.path.join(self.data_dir, file_name)
             data = {'img_path': img_path, 'label': label}
             if not os.path.exists(img_path):
